63a90864c453667ee47fcb82a8fc0f87598aad3c
kabysoft <kabysoft@yahoo.com>
2022-12-25 07:41:04 -0700

    Solution Exercise

---
+++ AnonymFunction/Anonym.js
@@ -0,0 +1,6 @@
+// this is an exemple of an anonymous Function 
+//function() {console.log('Iam anonymous Function)};
+let anonymousFunction = function() {
+    console.log('Iam anonymous Function')
+};
+anonymousFunction();
---
+++ Exercise/welcome.js
@@ -0,0 +1,14 @@
+function abracadabra(){
+let firstName = prompt('What is tour name please : ?');
+let lastName = prompt ('Insert your Last Name Please : ?');
+let age = prompt ('How Old Are You : ?')
+
+alert( 'Hello ! Mister  '
+ + lastName 
+ + '  Oh or the Greatest Scientist mister  : '
+ + firstName 
+ + '  The World Is iluminated By your name since  : ' 
+ + age + '  Years' )
+
+}
+abracadabra();
