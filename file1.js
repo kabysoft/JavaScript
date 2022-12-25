0ec26c8a96799a14504f143419720dfbcd9c3f3c
kabysoft <kabysoft@yahoo.com>
2022-12-25 06:26:25 -0700

    My Second Commit

---
+++ file1.js
@@ -0,0 +1,10 @@
+console.log('Hello Word i ts Great ');
+if (confirm('this is an alert continue or no ?')){
+    alert('You have confirmer ');
+    function sayHi() {
+        alert('Hello EveryBody');
+    };
+    sayHi();
+} else {
+    alert (' You have choos to cancel ');
+}
---
+++ function1.js
@@ -0,0 +1,5 @@
+function sayHi(message,firatName = 'Conected',lastName = 'As Guest') {
+    console.log(message,firatName,lastName);
+    
+};
+sayHi('Hello you status ' );
