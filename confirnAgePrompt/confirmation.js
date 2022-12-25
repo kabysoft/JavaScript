40dfde188de8f4d25cee7c0a115a252c3f4687cc
kabysoft <kabysoft@yahoo.com>
2022-12-25 06:57:24 -0700

    Add Concationation by parInt And Number, And ParsFloat

--- confirnAgePrompt/confirmation.js
+++ confirnAgePrompt/confirmation.js
@@ -4,3 +4,20 @@ function confirm(params) {
     
 }
 confirm();
+
+function concat(params) {
+    let futurAge =   prompt('How Old Are you : ?');
+    alert('you will have ' + (futurAge+1) +' Next Year');
+     // This COncatination give ass as a result 301 
+  }
+concat();
+
+function concat1(params) {
+    let futurAge =   prompt('How Old Are you : ?');
+    let nextAge = parseInt(futurAge)
+    alert('you will have ' + (nextAge+1) +' Next Year');
+     /* This COncatination give ass as a result 31 
+     also we can do this concatination width the function Number()
+     let nextAge = Number(futurAge);*/
+    }
+concat1();
